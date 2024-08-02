9936c954661d8d55b210fc0024c8115a8cbd01ec
joaozinhoevitinhodoisbaitolinha <161728394+joaozinhoevitinhodoisbaitolinha@users.noreply.github.com>
2024-08-02 09:27:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+
+const perguntas = [//Abre a lista de Objetos (itens)
+    {//Abre o item
+        enunciado:"Pergunta 1" ,
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+
+
+    }
+
+]
