b03a7bfef8e5702c8afe30ff89c35bffe244c566
joaozinhoevitinhodoisbaitolinha <161728394+joaozinhoevitinhodoisbaitolinha@users.noreply.github.com>
2024-08-23 09:17:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPergunta = document.querySelector('.caixa-pergunta');
-const caixaResultado = document.querySelector('.caixa-resultado');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
 
-const perguntas = [//Abre a lista de Objetos (itens)
-    {//Abre o item
-        enunciado:"Pergunta 1" ,
-        alternativas: ["Alternativa 1","Alternativa 2"]
+const perguntas = [//abre a lista de objetos (itens)
+    {//abre o item
+        enunciado:"Pergunta 1",
+        alternativas:["Alternativa 1","Alternativa 2"]
     },
-{//Abre o item
-    enunciado:"Pergunta 2" ,
-    alternativas: ["Alternativa 1","Alternativa 2"]
-},
-{//Abre o item
-    enunciado:"Pergunta 3" ,
-    alternativas: ["Alternativa 1","Alternativa 2"]
-},
+    {
+        enunciado:"Pergunta 2",
+        alternativas:["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado:"Pergunta 3",
+        alternativas:["Alternativa 1","Alternativa 2"]
+    }
 ]
-let posicao= 0;
-let perguntaAtual; 
+let posicao = 0;
+let perguntaAtual;
 
-function mostraPergunta (){
+function mostraPergunta(){
     
 }
