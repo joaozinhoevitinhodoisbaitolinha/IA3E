f73c28663743babee538ab65076e19ab4656a848
joaozinhoevitinhodoisbaitolinha <161728394+joaozinhoevitinhodoisbaitolinha@users.noreply.github.com>
2024-10-11 09:22:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,71 +2,69 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
 
-const perguntas = [//abre a lista de objetos (itens)
-    {//abre o item
-        enunciado: "Você gosta da Inteligência Artificial?",
-        alternativas: [{
-            texto: "Sim",
-            afirmação: "Afirmação da alternativa 1"
-        },
-        {
-            texto: "Não",
-            afirmação: "Afirmação da alternativa"
-        }
-        ]
+const perguntas = [     //serve para abrir lista de perguntas
+    {   //abre o objeto das perguntas
+        enunciado: "Você frequenta academia?",
+        alternativas: [
+            {texto: "Sim",
+            afirmação:"Parabéns pelo esforço"}, 
+
+            {texto: "Não",
+            afirmação:"Você deveria se esforçar mais"}]
     },
-    {
-        enunciado: "Pergunta 2",
-        alternativas: [{
-            texto: "Quem sabe...",
-            afirmação: "Afirmação da alternativa 1"
-        },
-        {
-            texto:"Talvez",
-            afirmação:"Afirmação da alternativa 2"
-        }
-        ]
+    { 
+        enunciado: "Você costuma fazer exercício aerobicos?",
+        alternativas: [
+            {texto: "Sim",
+            afirmação:"Você está no caminho certo"}, 
+                
+            {texto: "Não",
+            afirmação:"Você deveria começar a fazer"}]
     },
-    {
-        enunciado: "Pergunta 3",
-        alternativas: [{
-            texto: "Sim",
-            afirmação: "Afirmação da alternativa 1"
-        },
-        {
-            texto: "Não",
-            afirmação: "Afirmação da alternativa"
-        }
-        ]
-    }
+    { 
+        enunciado: "Você está satisfeito com seu corpo?",
+        alternativas: [
+            {texto: "Sim",
+            afirmação:"Fico feliz por você"}, 
+                
+            {texto: "Não",
+            afirmação:"Não fique triste, levante a cabeça e corra atrás de alcançar o seu corpo ''ideal'' "}]
+    },
 ]
 let posicao = 0;
 let perguntaAtual;
-let respostas = ""; 
+let respostas = "";
+
 
 function mostraPergunta() {
+    if (posicao>=perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[posicao];
     caixaPergunta.textContent = perguntaAtual.enunciado;
+    caixaAlternativa.textContent = " ";
     mostraAlternativas();
 }
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click",  () => respostasSelecionadas (alternativa));
+        botaoAlternativas.addEventListener("click",  () => respostasSelecionadas(alternativa));
         caixaAlternativa.appendChild(botaoAlternativas);
     }
 }
 function respostasSelecionadas(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmação
+    const afirmacoes = opcaoSelecionada.afirmação;
     respostas += afirmacoes + " ";
     posicao++;
     mostraPergunta();
-} 
+}
 function mostraResultado(){
-    caixaPergunta.textContent = "Daqui 10 anos ou até menos eu vou ser muito rico";
-    textoResultado.textContent = respostas;
-    caixaAlternativa.textContent = ""; 
+    caixaPergunta.textContent = "Confira suas respostas: ";
+    textoResultado.textContent = respostas; 
+    caixaAlternativa.textContent = "";
 }
 mostraPergunta();
